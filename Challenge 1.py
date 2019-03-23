3569c38bef5157982c2e083c163588288b70ff99
Duncan P <duncanpiscopo@gmail.com>
2019-03-23 01:11:35 +0100

    Create Challenge 1.py

---
+++ Challenge 1.py
@@ -0,0 +1,15 @@
+ez = "990" 
+iterations = int(2)
+
+
+def simpleRecursion(n, input):
+    if n > 0:
+        ezList = [str(int(i) +1) for i in input] #creating list from input whilst using a counter and type casting
+        output = ''.join(ezList)
+        print(output)
+        n -= 1
+        simpleRecursion(n,output)
+    if n <= 0:
+        print(output)
+        return
+simpleRecursion(iterations,ez)
