d28bb206ae8e3654280f9085c1dbafe98e702238
Duncan P <duncanpiscopo@gmail.com>
2019-03-25 22:45:59 +0100

    completed 1st part of 2nd challenge

--- challenge_2.py
+++ challenge_2.py
@@ -19,9 +19,31 @@ def collatz_sequence_recursion(input, resultList = None):
         return resultList
     if input % 2 > 0:
         input = ((3 * input) + 1)
-        collatz_sequence_recursion(input,resultList)
+        return collatz_sequence_recursion(input,resultList)
     else:
         input = (input / 2)
-        collatz_sequence_recursion(input,resultList)
+        return collatz_sequence_recursion(input,resultList)
+
+def collatz_sequence_recursion_glide(input, resultList = None):
+    if resultList is None:
+        resultList = []
+    resultList.append(input)
+    if input == 1:
+        return resultList
+    if input % 2 > 0:
+        input = ((3 * input) + 1)
+        return collatz_sequence_recursion(input,resultList)
+    else:
+        input = (input / 2)
+        return collatz_sequence_recursion(input,resultList)
         
 print(collatz_sequence_recursion(7))
+
+
+def glide_calc(resultList):
+    input = resultList[0]
+    while 
+    return count
+    
+    
+glide_calc(collatz_sequence_recursion(15))
